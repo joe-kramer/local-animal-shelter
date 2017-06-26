8d6bc83028a732ee37961f9b384d82b7b8efcfdd
Joe Kramer <joekramer1125@gmail.com>
2017-06-26 13:21:38 -0700

    add App.java and initialize 3 Animal objects

---
+++ App.java
@@ -0,0 +1,21 @@
+import java.io.Console;
+
+public class App {
+  public static void main(String[] args) {
+    Animal dog = new Animal();
+    dog.mSpecies = "dog";
+    dog.mName = "Scottie";
+    dog.mAge = 5;
+
+    Animal cat = new Animal();
+    cat.mSpecies = "cat";
+    cat.mName = "Franky";
+    cat.mAge = 3;
+
+    Animal bird = new Animal();
+    bird.mSpecies = "bird";
+    bird.mName = "Ally";
+    bird.mAge = 2;
+
+  }
+}
